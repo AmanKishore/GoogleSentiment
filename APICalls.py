46ddd74d9fbaff203e3c42b58db71838fa7b9a8c
Aman Kishore <amankishore105@gmail.com>
2019-01-03 17:00:33 -0500

    Updated to search with anything

--- APICalls.py
+++ APICalls.py
@@ -13,17 +13,23 @@ import TextAnalysisAPI
 import ast
 from WebScraper import scraper
 
-text = TextAnalysisAPI.ApiClient('1f67147b09804f90a1be856eaf11f89f')
+def sentiment(keyword):
 
-dat = scraper('https://www.google.com/search?q=facebook&rlz=1C1CHBF_enUS823US824&oq=facebook&aqs=chrome..69i57j69i60j0l4.4870j0j4&sourceid=chrome&ie=UTF-8')
+        text = TextAnalysisAPI.ApiClient('1f67147b09804f90a1be856eaf11f89f')
 
 
-total = 0
-elements = 0
-for x in range(len(dat)):
-        r = text.get_sentiment(dat[x])
-        retval = (ast.literal_eval(r.text))['documents']
-        if(retval):
-                total += (retval[0])['score']
-                elements += 1
-print(total/elements)
+        dat = scraper('https://www.google.com/search?q=' + keyword)
+
+
+        total = 0
+        elements = 0
+        for x in range(len(dat)):
+                r = text.get_sentiment(dat[x])
+                retval = (ast.literal_eval(r.text))['documents']
+                if(retval):
+                        total += (retval[0])['score']
+                        elements += 1
+        retstring = 'The sentiment of: ' +  keyword + ' is ' + str(total/elements)
+        return retstring
+
+print(sentiment('Trump'))
